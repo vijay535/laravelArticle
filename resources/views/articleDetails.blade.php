ad489d730aab36a9dba32b0879b2d9b0e570422b
Vijay Kumar <vk99938@gmail.com>
2021-02-19 03:36:45 +0530

    front file changed

--- resources/views/articleDetails.blade.php
+++ resources/views/articleDetails.blade.php
@@ -8,6 +8,7 @@
 	      <div class="card mb-4 box-shadow">
 	        <img class="card-img-top" style="height: auto; width: 100%;" src="http://localhost/laravelArticle/public/images/1613680524.png" data-holder-rendered="true">
 	        <div class="card-body">
+	          <h3>{{ $findArticleDetails->title }}</h3>
 	          <small class="text-muted">Tag: {{ $findArticleDetails->tag }}</small>
 	          <?php echo $findArticleDetails->description; ?>
 	          <div class="d-flex justify-content-between align-items-center">
--- resources/views/frontEndHome.blade.php
+++ resources/views/frontEndHome.blade.php
@@ -8,12 +8,13 @@
 	    <div class="col-md-4">
 	      <div class="card mb-4 box-shadow">
 	        <img class="card-img-top" style="height: 225px; width: 100%; display: block;" src="public/images/{{ $row->image }}" data-holder-rendered="true">
-	        <div class="card-body">
-	          <small class="text-muted">Tag: {{ $row->tag }}</small> ||
-	          <?php echo substr($row->description, 0, 300); ?>
-	          <div class="d-flex justify-content-between align-items-center">
+	        <div class="card-body" style="text-align: center;">
+	        	<h3>{{ $row->title }}</h3>
+		        <small class="text-muted">Tag: {{ $row->tag }}</small> ||
+		        <?php echo substr($row->description, 0, 300); ?>
+	          	<div class="d-flex justify-content-between align-items-center">
 	            <div class="btn-group">
-	              <a href="articleDetails/{{ $row->id }}"class="btn btn-sm btn-outline-secondary">View Details</a>
+	              <a href="articleDetails/{{ $row->id }}"class="btn btn-sm btn-outline-secondary">Read More</a>
 	            </div>
 	            <!-- <small class="text-muted">9 mins</small> -->
 	          </div>
